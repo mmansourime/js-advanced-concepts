c26341901eed1c168a95229fbfa1f91bbd6302df
mmansourime <m.mansouri.me@gmail.com>
2022-09-02 12:29:04 +0430

    1401-06-11-12:30

---
+++ Try Catch.js
@@ -0,0 +1,16 @@
+    //Try Catch: مدیریت خطا
+
+try {
+    let num1 = 5;
+    let result;
+    result = num1 = 10;
+    console.log(sum);
+    
+} catch(error) {
+    // console.log("دوباره سعی نمایید!!!");
+    //console.log(error);
+    //console.log(error.name);
+    //console.log(error.message);
+    console.log(`${error.name}: Please Declare sum First`);
+}
+    console.log("Mohammad");
